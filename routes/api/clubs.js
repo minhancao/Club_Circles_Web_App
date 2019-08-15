01f2f1da27af6136ceeb676535cf821629330ad1
minhancao <minhan.duc.cao@gmail.com>
2019-08-14 18:02:10 -0700

    Added editting endpoint for announcements

--- routes/api/clubs.js
+++ routes/api/clubs.js
@@ -44,11 +44,36 @@ router.delete("/:id", (req, res) => {
 });
 
 // update club
-// announcement
-router.put("/:id", (req, res) => {
+// This is for adding to announcements, events, etc.
+router.put("/add/:id", (req, res) => {
   Club.findOneAndUpdate(
     { _id: req.params.id },
     { $push: req.body },
+    { sort: { _id: -1 }, upsert: true },
+    (err, result) => {
+      if (err) return res.json(err);
+    }
+  )
+    .then(item => res.json(item))
+    .catch(err => res.status(404).json({ success: false }));
+});
+
+// update club
+// This is for editting announcements
+/* Input format ex: 
+{
+	"name": "omegalul12335", "announcement": "hey there35354"
+}
+*/
+router.put("/edit/announcements/:id/:id2", (req, res) => {
+  Club.findOneAndUpdate(
+    { _id: req.params.id, "announcements._id": req.params.id2 },
+    {
+      $set: {
+        "announcements.$.name": req.body.name,
+        "announcements.$.announcement": req.body.announcement
+      }
+    },
     { sort: { _id: -1 }, upsert: true },
     (err, result) => {
       if (err) return res.json(err);
